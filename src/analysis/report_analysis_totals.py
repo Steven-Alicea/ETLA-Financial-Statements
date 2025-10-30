84a5e8c680d8291bb7d584ad213006e702302230
Steven Alicea <Steven.M.Alicea@gmail.com>
2025-10-30 13:22:22 -0700

    feat: add report generation and helpers
    - add report generation for total count and amount of transactions
    
    - add report helper fuctions

---
+++ src/analysis/report_analysis_totals.py
@@ -0,0 +1,30 @@
+from src.analysis.report_helpers import *
+from src.globals.functions import create_directory, join_directory_and_file
+from src.etl.load.load import load_to_csv
+
+
+
+target_directory = "data/reports"
+
+
+def print_totals_report(df, title):
+    df = format_dataframe_for_totals_report(df)
+    count_total = get_total_count(df)
+    amount_total = get_total_amount(df)
+    df = format_dataframe_for_totals_report(df, currency=True)
+    print(title)
+    print(df)
+    print(f"{count_total[0]} = {count_total[1]}")
+    print(f"{amount_total[0]} = {amount_total[1]}")
+
+def generate_totals_csv_report(df, title):
+    df = format_dataframe_for_totals_report(df)
+    count_total = get_total_count(df)
+    amount_total = get_total_amount(df)
+    df = format_dataframe_for_totals_report(df, currency=True)
+    file_name = f"{title}.csv"
+    create_directory(target_directory)
+    csv_file = join_directory_and_file(target_directory, file_name)
+    load_to_csv(csv_file, df)
+    write_totals_to_csv(csv_file, count_total, amount_total)
+    print(f"Report: {csv_file}")
---
+++ src/analysis/report_helpers.py
@@ -0,0 +1,25 @@
+import csv
+
+
+
+def write_totals_to_csv(csv_file, count_total, amount_total):
+    with open(csv_file, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([])
+        writer.writerow(count_total)
+        writer.writerow(amount_total)
+
+def format_dataframe_for_totals_report(df, currency=None):
+    if currency:
+        df.loc[:, "Amount"] = '$' + df["Amount"].astype("string")
+    else:
+        df = df[["Statement Period", "Transaction Date", "Description", "Amount"]]
+        df.loc[:, "Amount"] = df["Amount"].abs()
+        df = df.sort_values(by="Transaction Date", ascending=False)
+    return df
+
+def get_total_count(df):
+    return [f"Total Transactions", len(df)]
+
+def get_total_amount(df):
+    return [f"Total Amount", f"${df["Amount"].sum()}"] 
