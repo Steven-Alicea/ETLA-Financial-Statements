833ed0e3f046b62c7b3ce9967e4cd7168b17871e
Steven Alicea <Steven.M.Alicea@gmail.com>
2025-10-30 14:08:10 -0700

    feat: add formating for report retrieval

--- src/analysis/get_csv_report_dataframe.py
+++ src/analysis/get_csv_report_dataframe.py
@@ -8,4 +8,6 @@ directory = "reports"
 
 def get_csv_report(file_name):
     report_file = join_directory_and_file(directory, file_name)
-    return pd.read_csv(report_file, skipfooter=3, engine='python')
+    df = pd.read_csv(report_file, skipfooter=3, engine='python')
+    df["Amount"] = pd.to_numeric(df["Amount"].str.strip('$'))
+    return df
