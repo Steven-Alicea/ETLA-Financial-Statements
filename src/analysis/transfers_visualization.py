771c290a11228d0e12805d277258542f8318c051
Steven Alicea <Steven.M.Alicea@gmail.com>
2025-11-08 16:34:53 -0800

    refac: correct errors
    - correct matplotlib pyplot module import
    
    - correct target directory
    
    - make hue optional

--- src/analysis/transfers_visualization.py
+++ src/analysis/transfers_visualization.py
@@ -1,13 +1,13 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from globals.functions import create_directory, join_directory_and_file
 
 
 
-target_directory = "visualizations"
+target_directory = "data/visualizations"
 
 
-def generate_transfers_visualization(df, title, x_axis, y_axis_, hue):
+def generate_transfers_visualization(df, title, x_axis, y_axis_, hue=None):
     sns.set_theme(style='whitegrid', color_codes=True)
     sns_barplot = sns.barplot(data=df, x=x_axis, y=y_axis_, hue=hue, estimator=sum, errorbar=None)
     sns_barplot.set_title(title)
