61ece378ab28d1e380b2c0163f9e73d14c7b7074
Steven Alicea <Steven.M.Alicea@gmail.com>
2025-11-17 18:01:29 -0800

    feat: add toyota and wells fargo functions
    - add formatting for toyota statements and transation history analysis
      dataframes
    
    - add formatting for wells fargo tesla transaction history analysis
      dataframe

--- src/analysis/format_dataframes.py
+++ src/analysis/format_dataframes.py
@@ -148,3 +148,50 @@ def format_nfcu_navcheck_transaction_dataframe(df):
     df["Transaction Date"] = pd.to_datetime(df["Transaction Date"])
     df[decimal_columns] = df[decimal_columns].map(lambda x: Decimal(f"{x:.2f}"))
     return df
+
+
+# format Tesla dataframes
+def format_toyota_transaction_dataframe(df):
+    df["Transaction Date"] = pd.to_datetime(df["Transaction Date"])
+    df["Status"] = df["Status"].astype("string")
+    df["Amount"] = pd.to_numeric(df["Amount"]).apply(lambda x: Decimal(f"{x:.2f}"))
+    return df
+
+def format_toyota_statement_dataframe(df):
+    date_columns = ["Statement Date",
+                    "Payment Due Date",
+                    "Date of Last Transaction",
+                    "Maturity Date"]
+    decimal_columns = ["Past Due Payment Amount", 
+                       "Unpaid Late Charges",
+                       "Miscellaneous Charges",
+                       "Current Payment Due",
+                       "Total Amount Due",
+                       "Regular Payment Amount",
+                       "Last Transaction Amount",
+                       "Monthly Payments Made",
+                       "Outstanding Balance"]
+    df[decimal_columns] = df[decimal_columns].map(lambda x: Decimal(f"{x:.2f}"))
+    df[date_columns] = df[date_columns].apply(lambda x: pd.to_datetime(x, format='%Y-%m-%d'))
+    df["Monthly Payments Made"] = df["Monthly Payments Made"].astype(int)
+    return df
+
+
+# format Wells Fargo Tesla dataframe
+def format_wells_fargo_tesla_transaction_dataframe(df):
+    date_columns = ["Posting Date", "Next Due Date"]
+    decimal_columns = ["Transaction Amount", 
+                       "Principal",
+                       "Interest", 
+                       "CPI", 
+                       "CPI Interest", 
+                       "Late Charges", 
+                       "Other Charges", 
+                       "Unapplied Amount", 
+                       "CPI Balance", 
+                       "Principal Balance", 
+                       "Payment Variance"]
+    df[decimal_columns] = df[decimal_columns].map(lambda x: Decimal(f"{x:.2f}"))
+    df[date_columns] = df[date_columns].apply(lambda x: pd.to_datetime(x, format='%Y-%m-%d'))
+    df["Transaction Description"] = df["Transaction Description"].astype("string")
+    return df
